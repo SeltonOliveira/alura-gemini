dcedb65f24d9df1052f3daa95260e8e39e70773f
SeltonOliveira <selllpw@hotmail.com>
2024-09-11 02:23:33 -0300

    add-pokemon

--- js/dados.js
+++ js/dados.js
@@ -21,5 +21,53 @@ let dados = [{
     descricao : 'Após o nascimento, suas costas incha e endurece formando uma concha. Ele espalha uma espuma potente pela boca.',
     imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
     link : 'https://www.pokemon.com/br/pokedex/squirtle'
+},
+{
+    nome : 'Pikachu',
+    tipo : 'Elétrico',
+    fraqueza: 'Ground',
+    descricao : 'Quando está irritado, descarrega imediatamente a energia armazenada nas bolsas em suas bochechas.',
+    imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png',
+    link : 'https://www.pokemon.com/br/pokedex/pikachu'
+},
+{
+    nome : 'Psyduck',
+    tipo : 'Água',
+    fraqueza: 'Grama e Eletricidade',
+    descricao : 'É constantemente assolado por uma dor de cabeça. Quando a dor de cabeça fica intensa, ela começa a usar poderes misteriosos.',
+    imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/054.png',
+    link : 'https://www.pokemon.com/br/pokedex/psyduck'
+},
+{
+    nome : 'Cubone',
+    tipo : 'Ground',
+    fraqueza: 'Água, Grama e Gelo',
+    descricao : 'Quando a memória de sua falecida mãe o leva às lágrimas, seus gritos ecoam tristemente dentro do crânio que ele usa na cabeça.',
+    imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/104.png',
+    link : 'https://www.pokemon.com/br/pokedex/cubone'
+},
+{
+    nome : 'Mew',
+    tipo : 'Psíquico',
+    fraqueza: 'Fantasma e Escuridão',
+    descricao : 'Quando visto através de um microscópio, o cabelo curto, fino e delicado deste Pokémon pode ser visto.',
+    imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/151.png',
+    link : 'https://www.pokemon.com/br/pokedex/mew'
+},
+{
+    nome : 'Mewtwo',
+    tipo : 'Psíquico',
+    fraqueza: 'Fantasma e Escuridão',
+    descricao : 'Seu DNA é quase igual ao de Mew. No entanto, seu tamanho e disposição são muito diferentes.',
+    imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/150.png',
+    link : 'https://www.pokemon.com/br/pokedex/mewtwo'
+},
+{
+    nome : 'Togepi',
+    tipo : 'Fada',
+    fraqueza: 'Veneno e Aço',
+    descricao : 'A concha parece estar cheia de alegria. Diz-se que ele compartilhará boa sorte quando tratado com gentileza.',
+    imagem : 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/175.png',
+    link : 'https://www.pokemon.com/br/pokedex/togepi'
 }
 ]
